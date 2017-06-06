c3aad2907b113f00c5ab4986f8740b91bd839d36
Juca Azevedo <juca.azevedo@gmail.com>
2017-06-06 20:40:13 -0300

    mais

--- codigo.py
+++ codigo.py
@@ -4,13 +4,19 @@ def func1(nome):
 
 	print ("tamanho de aa ",len(aa.text))
 	with open(nome,"r") as file:
-	for line in file:
-		words = line.split()
-		print("words = ",words)
-		print(words[0])
-		print(len(words))
-		print(len(words[0]))
+		for line in file:
+			words = line.split()
+			print("words = ",words)
+			print(words[0])
+			print(len(words))
+			print(len(words[0]))
+
+def func2():
+	if True:
+		print(1)
+print(2)
 
 func1("arq.txt")
+func2()
 
 
