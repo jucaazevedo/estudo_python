4b066034788c822213c602fa72b47b4f47db0ba8
Joaquim Azevedo <jazevedo@simplesmenteuse.com>
2019-08-22 10:43:17 -0300

    novas

--- escaneia.py
+++ escaneia.py
@@ -2,10 +2,13 @@ def escaneia():
 	import json
 	f = open("AutomationAPISampleFlow.json")
 	l = f.read()
+	print("texto:")
 	print(l)
 	print("")
 	j = json.loads(l)
 	print(type(j))
+	print("dictionary::")
+	print(j)
 	print(j['Defaults']['Application'])
 
 escaneia()
